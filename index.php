f2c9125e407dc2a666f781e1cc6f50ec85c04d3c
Cannon431 <pokazanov14@gmail.com>
2017-08-12 17:53:15 +0600

    Test

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//Justify Framework v0.1
+//Justify Framework v0.1b
 define('BASE_DIR', getcwd());
 define('TWIG_CACHE_DIR', BASE_DIR . '/framework/cache');
 require_once BASE_DIR . '/framework/starter.php';
