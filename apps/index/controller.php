ecdbc0617b27dd7e566b3e04de7c4987a6364106
Cannon431 <pokazanov14@gmail.com>
2017-08-12 17:10:54 +0600

    Main page changes

--- apps/index/controller.php
+++ apps/index/controller.php
@@ -4,7 +4,10 @@ class IndexController extends Controller
 {
     public function actionViewMainPage()
     {
-        render('index', array());
+        render('index', [
+            'frameworkName' => 'Justify Framework',
+            'frameworkVersion' => 'v0.1b'
+        ]);
         return true;
     }
 }
