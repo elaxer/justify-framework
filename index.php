3db1ca46f8c2cfc00c4215c132ef3624ad9beff3
Cannon431 <pokazanov14@gmail.com>
2017-08-13 13:54:34 +0600

    Small changes

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 //Justify Framework v0.1b
 define('BASE_DIR', getcwd());
 define('TWIG_CACHE_DIR', BASE_DIR . '/framework/cache');
+
 require_once BASE_DIR . '/framework/starter.php';
 
 $app = new Justify;
 $app->run();
+$result = fill_arithmetic_progression(12, 4);
+dump($result);
