de0f5aaf7e26a95679af5024b54d7a3e785709b6
Alexey Pokazanov <pokazanov14@gmail.com>
2017-08-19 11:53:11 +0600

    New date module

---
+++ framework/modules/date.module.php
@@ -0,0 +1,43 @@
+<?php
+
+function return_russian_month(int: $mounth)
+{
+	if ($mounth == 1) {
+		return 'января';
+	}
+	if ($mounth == 2) {
+		return 'февраля';
+	}
+	if ($mounth == 3) {
+		return 'марта';
+	}
+	if ($mounth == 4) {
+		return 'апреля';
+	}
+	if ($mounth == 5) {
+		return 'мая';
+	}
+	if ($mounth == 6) {
+		return 'июня';
+	}
+	if ($mounth == 7) {
+		return 'июля';
+	}
+	if ($mounth == 8) {
+		return 'августа';
+	}
+	if ($mounth == 9) {
+		return 'сентября';
+	}
+	if ($mounth == 10) {
+		return 'октября';
+	}
+	if ($mounth == 11) {
+		return 'ноября';
+	}
+	if ($mounth == 12) {
+		return 'декабря';
+	}
+
+	return false;
+}
