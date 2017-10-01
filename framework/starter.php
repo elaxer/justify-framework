3e623b2106bc8cbe00ad6ae9478ba8c7abb9592d
Cannon431 <pokazanov14@gmail.com>
2017-10-01 18:20:04 +0600

    -

--- framework/starter.php
+++ framework/starter.php
@@ -1,16 +1,12 @@
 <?php
-/**
- * Loads requires files
- */
+//Loads requires files
 $start = microtime(true);
 
 if (file_exists(BASE_DIR . '/framework/core/composer/vendor/autoload.php')) {
     require_once BASE_DIR . '/framework/core/composer/vendor/autoload.php';
 }
 
-/**
- * Loads modules
- */
+//Loads modules
 $modules = glob(BASE_DIR . '/framework/modules/*.module.php');
 foreach ($modules as $module) {
     require_once $module;
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -4,9 +4,9 @@
 <head>
     <meta charset="<?= $charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="<?= @$description ?>">
-    <meta name="author" content="<?= @$author ?>">
-    <meta name="keywords" content="<?= @$keywords ?>">
+    <meta name="description" content="<?= $description ?>">
+    <meta name="author" content="<?= $author ?>">
+    <meta name="keywords" content="<?= $keywords ?>">
 
     <title><?= $title ?></title>
 
