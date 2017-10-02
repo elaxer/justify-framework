78e91f0332e2f25d112aefed24603c6af686e554
Cannon431 <pokazanov14@gmail.com>
2017-10-02 19:19:07 +0600

    Small fixes

--- config/db.php
+++ config/db.php
@@ -1,7 +1,7 @@
 <?php
 return array(
-    'host' => '', //Data base host
-    'user' => '', //Data base user name
+    'host' => '127.0.0.1', //Data base host
+    'user' => 'root', //Data base user name
     'password' => '', //User password
     'name' => '', //Data base name
     'charset' => 'utf8' //Data base charset
