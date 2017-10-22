0dd71f6cef66250c78460ca0d4876503b0fde724
Cannon431 <pokazanov14@gmail.com>
2017-10-22 17:56:39 +0600

    Small changes

--- index.php
+++ index.php
@@ -15,7 +15,3 @@ require_once BASE_DIR . '/framework/justify/Justify.php';
 $settings = require_once CONFIG_DIR . '/settings.php';
 
 (new justify\framework\App($settings))->run();
-
-$email = 'pokazanov14@gmail.com';
-//echo preg_match('/^\S+@\S+\.\S+$/i', $email) ? 'ОК!' : 'Введите корректный E-mail!';
-echo preg_match('/jpe?g$/i', 'file.jpeg');
